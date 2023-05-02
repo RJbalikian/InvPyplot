c592da6ae8fed6955ac47b2366c463b96be9bdc6
RJbalikian <46536937+RJbalikian@users.noreply.github.com>
2023-05-02 11:52:57 -0500

    update description and 'force arg

--- docs/generate_docs_pdoc.py
+++ docs/generate_docs_pdoc.py
@@ -1,3 +1,4 @@
+#This script generates simple documentation using the pdoc3 library that can easily be published on github pages
 import os
 import subprocess
 
@@ -8,6 +9,6 @@ output_dir = 'docs'
 os.environ['PYTHONPATH'] = '..' + os.pathsep + os.environ.get('PYTHONPATH', '')
 
 # Run the pdoc command
-subprocess.run(['pdoc3', '--html', mod, '-o', output_dir, --force])
+subprocess.run(['pdoc3', '--html', mod, '-o', output_dir, '--force'])
 os.chdir('./docs')
 os.rename(mod[:-3]+'.html', 'index.html')
