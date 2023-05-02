09901622ff1371d97439d693525ea7c6ed64348a
RJbalikian <46536937+RJbalikian@users.noreply.github.com>
2023-05-02 11:46:23 -0500

    update index file

--- docs/generate_docs_pdoc.py
+++ docs/generate_docs_pdoc.py
@@ -8,4 +8,6 @@ output_dir = 'docs'
 os.environ['PYTHONPATH'] = '..' + os.pathsep + os.environ.get('PYTHONPATH', '')
 
 # Run the pdoc command
-subprocess.run(['pdoc3', '--html', mod, '-o', output_dir])
+subprocess.run(['pdoc3', '--html', mod, '-o', output_dir, --force])
+os.chdir('./docs')
+os.rename(mod[:-3]+'.html', 'index.html')
